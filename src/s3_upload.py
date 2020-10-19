4ed22c46b39c9436d914c6f2b1d418b2092aa267
Rupert Adams <RupertNCAdams@aol.com>
2020-10-19 17:08:26 +0200

    updated s3_upload with os.environ

--- src/s3_upload.py
+++ src/s3_upload.py
@@ -1,11 +1,15 @@
 import boto3
+import os
+
+aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 s3 = boto3.resource(
     service_name='s3',
     region_name='eu-west-1',
-    aws_access_key_id='mykey',
-    aws_secret_access_key='mysecretkey'
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key
 )
 
 if __name__ == "__main__":
-    s3.Bucket('Ru_apps_table_bucket').upload_file(Filename='apps.csv', Key='apps.csv')
+    s3.Bucket('Ru_apps_table_bucket').upload_file(Filename='apps.csv.gzip', Key='apps.csv.gzip')
